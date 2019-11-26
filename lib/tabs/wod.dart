547d719667686b045a320fb93455be952d9a6d76
digitalartes <felipe.digitalartes@hotmail.com>
2019-11-26 19:07:21 -0300

    wod html reformulada

--- lib/tabs/wod.dart
+++ lib/tabs/wod.dart
@@ -5,51 +5,37 @@ class Wod extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Html(
-        data:"""
+      padding: EdgeInsets.fromLTRB(10, 0, 10, 15),
+      child: Html(data: """
         
-        <div>  
-        <h2>Aquecimento</h2>
-
+      
+        <h5>Aquecimento</h5>
+      
+        <hr />
           <p>4' for Mobolity </p>
           <p>4' Amrap </p>
-        <br>
         <br>
         <span>- 15 Flexões Plantares </span>
         <span>- 10 Sit Ups</span>
         <span>- 20" HS Hold </span>
-         <hr />
-
         <p> Strenght</p>
-
         <p> Cap 5' </p>
         <span>- 3x10 Remada Curvada (Carga Livre) </span>
+         
 
-        </div> 
-
-
-        <div> 
-        <h2> Técnica</h2>
-
+       <h5> Técnica</h5>
+        <hr>
         <p> 15' - Push Press </p>
+        
+        
 
-        </div>
-
-
-        <div> 
-        <h2> Wod </h2>
+        <h5> Wod </h5>
+     
+        <hr>
         <p> Cap 14'</p>
         <span>200 D.u/400 S.U</span>
         <span>1,6 Km Run</span>
-        
-        <hr>
-        
-        
-        
-        </div>
 
-
-         """)
-    );
+         """));
   }
 }
